258e37a77476979fd328972ef9a1d252f62f5695
Daniil Dotsev <dddotsev@gmail.com>
2018-04-29 17:44:37 +0300

    Fix bugs and add config

---
+++ config.py
@@ -0,0 +1,19 @@
+MAIN_URL = 'https://apod.nasa.gov/apod/'
+MAIN_PAGE = 'archivepix.html'
+
+DOWNLOAD_PATH = '/data/data/com.termux/files/home/storage/external-1/NasaImages/'
+DOWNLOAD_PATH_UNMODIFIED = '/data/data/com.termux/files/home/storage/external-1/NasaImagesUnmodified/'
+DOWNLOADED_LIST_FILE_PATH = 'downloaded.txt'
+IMAGE_NOT_FOUND_FILE_PATH = 'image_not_found.txt'
+
+LOADED_IMAGE_LINKS_FILE_PATH = 'loaded_links.json'
+
+IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'bmp', 'png', 'tiff', 'tif']
+
+REQUEST_REPEAT_COUNT = 15
+REQUEST_REPEAT_INITIAL_SECONDS = 5
+
+TAKE = 5
+SKIP = 20
+
+LOADED = False
--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 from os import makedirs
 from os.path import exists
-
 from time import sleep
 from urllib.request import urlopen
 from urllib.error import URLError
 from json import load, dumps
+from io import BytesIO
 
 from bs4 import BeautifulSoup
 from progressbar import ProgressBar
-
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-from io import BytesIO
+import config
 
-MAIN_URL = 'https://apod.nasa.gov/apod/'
-MAIN_PAGE = 'archivepix.html'
+MAIN_URL = config.MAIN_URL
+MAIN_PAGE = config.MAIN_PAGE
 
-DOWNLOAD_PATH = '/data/data/com.termux/files/home/storage/external-1/NasaImages/'
-DOWNLOAD_PATH_UNMODIFIED = '/data/data/com.termux/files/home/storage/external-1/NasaImagesUnmodified/'
-DOWNLOADED_LIST_FILE_PATH = 'downloaded.txt'
-IMAGE_NOT_FOUND_FILE_PATH = 'image_not_found.txt'
+DOWNLOAD_PATH = config.DOWNLOAD_PATH
+DOWNLOAD_PATH_UNMODIFIED = config.DOWNLOAD_PATH_UNMODIFIED
+DOWNLOADED_LIST_FILE_PATH = config.DOWNLOADED_LIST_FILE_PATH
+IMAGE_NOT_FOUND_FILE_PATH = config.IMAGE_NOT_FOUND_FILE_PATH
 
-LOADED_IMAGE_LINKS_FILE_PATH = 'loaded_links.json'
+LOADED_IMAGE_LINKS_FILE_PATH = config.LOADED_IMAGE_LINKS_FILE_PATH
 
-IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'bmp', 'png', 'tiff', 'tif']
+IMAGE_EXTENSIONS = config.IMAGE_EXTENSIONS
 
-REQUEST_REPEAT_COUNT = 15
-REQUEST_REPEAT_INITIAL_SECONDS = 5
+REQUEST_REPEAT_COUNT = config.REQUEST_REPEAT_COUNT
+REQUEST_REPEAT_INITIAL_SECONDS = config.REQUEST_REPEAT_INITIAL_SECONDS
+
+TAKE = config.TAKE
+SKIP = config.SKIP
+
+LOADED = config.LOADED
 
 def get_image_pages(take, skip):
     html = get_request(MAIN_URL + MAIN_PAGE)
@@ -68,17 +72,17 @@ def get_image_links(image_pages):
 
                 if is_image_link(parent_href):
                     found_image = True
-                    found_image_in_a = True
+                    found_image_in_here = True
                 else:
                     parent_href = None
 
                 if is_image_link(img_src):
                     found_image = True
-                    found_image_in_a = True
+                    found_image_in_here = True
                 else:
                     img_src = None
 
-                if found_image_in_a:
+                if found_image_in_here:
                     image_links.append((page, parent_href, img_src))
 
         if not found_image:
@@ -104,26 +108,36 @@ def is_image_link(link):
 def download_images(image_links):
     progressbar = ProgressBar(0, len(image_links))
     downloaded_list_file = open(DOWNLOADED_LIST_FILE_PATH, "a")
+    image_not_found_file = open(IMAGE_NOT_FOUND_FILE_PATH, "a")
 
     bar_progress = 0
     for link in image_links:
         page, href, src = link
 
         downloaded = False
+        try:
+            if href is not None:
+                downloaded = download_image(page, href)
+            if src is not None and not downloaded:
+                downloaded = download_image(page, src)
 
-        if href is not None:
-            downloaded = download_image(page, href)
+            if downloaded:
+                write_and_flush(downloaded_list_file, page)
+            else:
+                write_and_flush(image_not_found_file, page)
+        except Exception as ex:
+            print("Exception while downloading " + page)
+            print_except(ex)
 
-        if src is not None and not downloaded:
-            downloaded = download_image(page, src)
-
-        downloaded_list_file.write('\n')
-        downloaded_list_file.write(page)
-        downloaded_list_file.flush()
         bar_progress += 1
         progressbar.update(bar_progress)
 
     downloaded_list_file.close()
+
+def write_and_flush(open_file, string):
+    open_file.write('\n')
+    open_file.write(string)
+    open_file.flush()
 
 def download_image(page, link):
     date = page[2:-5]
@@ -134,7 +148,6 @@ def download_image(page, link):
     image_req = get_request(MAIN_URL + link)
     if image_req == False:
         return False
-
 
     imageBytes = BytesIO(image_req)
 
@@ -218,12 +231,16 @@ def get_request(url):
                 print('Download retry num. ' + str(retry))
                 print('Trying to get: ' + url)
                 print('Retry in ' + str(sleep_time) + 'seconds')
-                print(ex.args)
-                print(ex.strerror)
-                print(ex)
+                print_except(ex)
                 sleep(sleep_time)
             else:
                 raise
+
+
+def print_except(ex):
+    print(ex.args)
+    print(ex.strerror)
+    print(ex)
 
 
 def write_links_to_file(links):
@@ -245,7 +262,7 @@ def load_links_from_file():
     return links
 
 
-def main(take, skip, loaded):
+def main():
     if not exists(DOWNLOAD_PATH):
         makedirs(DOWNLOAD_PATH)
 
@@ -260,8 +277,8 @@ def main(take, skip, loaded):
 
     links = None
 
-    if not loaded:
-        pages = get_image_pages(take, skip)
+    if not LOADED:
+        pages = get_image_pages(TAKE, SKIP)
         pages = exclude(pages)
         print("To download: " + str(len(pages)))
 
@@ -288,4 +305,4 @@ def main(take, skip, loaded):
 
     return
 
-main(10, 10, False)
+main()
